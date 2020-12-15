3a7e90d7a0334feb00cf02e1941863511cb16496
tjvneste <58135742+tjvneste@users.noreply.github.com>
2020-12-15 11:02:54 +0100

    transcription_final

--- Transcription profiling of human breast tumors and their paired normal tissues/Transcription_script.R
+++ Transcription profiling of human breast tumors and their paired normal tissues/Transcription_script.R
@@ -1,7 +1,6 @@
-#Transcription profiling of human breast tumors and their paired normal tissues
-#desktop
-setwd("~/Documents/Bioinformatics/Applied high-throughput analysis/project_AHTA/Transcription profiling of human breast tumors and their paired normal tissues")
+#set working directory 
 #https://www.ebi.ac.uk/arrayexpress/experiments/E-GEOD-15852/
+setwd("~/Documents/Bioinformatics/Applied high-throughput analysis/project_AHTA/Transcription profiling of human breast tumors and their paired normal tissues")
 
 ## Load packages
 library(affy)
@@ -44,24 +43,27 @@ for (i in pData(BreastCancer)$sample){
 disease
 patients
 annotation <- data.frame(disease,patients)
+annotation
 dim(exprs(BreastCancer))
 
 
 ## Quality Control on raw data
 ####################
-# this is used to perform an explorative quality evaluation of the orginal data and the log transformed data
-## arrayQualityMetrics (open "index.html" file for a full overview of the output)
-arrayQualityMetrics(BreastCancer,outdir="~/Documents/Bioinformatics/Applied high-throughput analysis/project_AHTA/Transcription profiling of human breast tumors and their paired normal tissues/raw",force=T)
-arrayQualityMetrics(BreastCancer,outdir="~/Documents/Bioinformatics/Applied high-throughput analysis/project_AHTA/Transcription profiling of human breast tumors and their paired normal tissues/rawlog",force=T,do.logtransform=T)
+# raw data
+arrayQualityMetrics(BreastCancer,outdir="~/Documents/Bioinformatics/Applied high-throughput analysis/project_AHTA/
+                    Transcription profiling of human breast tumors and their paired normal tissues/raw",force=T)
+# logtransformed data
+arrayQualityMetrics(BreastCancer,outdir="~/Documents/Bioinformatics/Applied high-throughput analysis/project_AHTA/
+                    Transcription profiling of human breast tumors and their paired normal tissues/rawlog",force=T,do.logtransform=T)
 
 # Preprocessing of the data 
-# rma function is used to perform background correction as well as quantile normalization 
-BreastCancerRMA<- affy::rma(BreastCancer,background=T) #  If TRUE, background correct using RMA background correction
+BreastCancerRMA<- affy::rma(BreastCancer,background=T)
 
 
 ## Quality Control on preprocessed data
 ## QC post preprocessing
-arrayQualityMetrics(BreastCancerRMA,outdir="~/Documents/Bioinformatics/Applied high-throughput analysis/project_AHTA/Transcription profiling of human breast tumors and their paired normal tissues/RMA",force=T)  			#RMA produces log-transformed data
+arrayQualityMetrics(BreastCancerRMA,outdir="~/Documents/Bioinformatics/Applied high-throughput analysis/project_AHTA/
+                    Transcription profiling of human breast tumors and their paired normal tissues/RMA",force=T)  			#RMA produces log-transformed data
 
 head(exprs(BreastCancerRMA))
 head(exprs(BreastCancer))
@@ -69,7 +71,7 @@ head(exprs(BreastCancer))
 annotation
 dim(annotation) # 86 2
 
-# this piece is only necessary if you use the object of ArrayExpress otherwise skip this
+# this piece is only necessary if you got the object of ArrayExpress otherwise skip this
 #annotb <- as.double(annot==annot[4]) # we want the breast tumor tissue to be one and the control to be zero 
 #annotb
 # the problem is we don't account for the persons but can we? bcs we have 86 samples? YES
@@ -91,10 +93,8 @@ annotation$disease <- factor(annotation$disease)
 annotation
 
 ## Differential expression by LIMMA
-# Method as stated in limma package (no intercept, easy for simple model designs)
 design <- model.matrix(~0+disease+patients, data= annotation)
-colSums(design) # checking if this is correct 
-#patients toevoegen
+colSums(design)
 colnames(design)[1:2]<-c("Cancer_tissue","normal_tissue")
 
 fit <- lmFit(BreastCancerRMA,design)
@@ -105,29 +105,33 @@ fit2 <- eBayes(fit2)
 LIMMAout <- topTable(fit2,adjust="BH",number=nrow(exprs(BreastCancerRMA))) 
 head(LIMMAout)
 
-limma::plotMA(fit2, main= 'MA-plot')
+# MA plot
+threshold.sign <- LIMMAout[LIMMAout$adj.P.Val<0.05,]
+dim(threshold.sign)
+with(LIMMAout, plot(AveExpr, logFC, pch=20,main="MA plot"))
+with(subset(threshold.sign),points(AveExpr, logFC,pch=20,col="red"))
+# histogram p-values
 hist(fit2$p.value, main= 'distributions of the p-values',xlab='p-values')
-
+# Volcano plot
 threshold.sign <- LIMMAout[LIMMAout$adj.P.Val<0.05,]
 dim(threshold.sign)
 with(LIMMAout, plot(logFC, -log10(P.Value), pch=20,main="Volcano plot"))
 with(subset(threshold.sign),points(logFC, -log10(P.Value),pch=20,col="red"))
 
      
-# Have a look at the results and search for other probesets for your DE genes
+# Have a look at the results 
 significant_pvalues<- LIMMAout[LIMMAout$adj.P.Val<0.05,]
 dim(significant_pvalues) # 5333 probes statistical significant
 significant_pvalues_1<- LIMMAout[LIMMAout$adj.P.Val<0.05 & abs(LIMMAout$logFC) >1,]
 dim(significant_pvalues_1) # 40 probes statistical significant 
 significant_pvalues_1
+
 significant_pvalues_2<- LIMMAout[LIMMAout$adj.P.Val<0.05 & abs(LIMMAout$logFC) >1.5,]
 dim(significant_pvalues_2) # 9 probes statistical significant 
 head(significant_pvalues_1)
 
 ## Load annotation and sort alphabetically on probe name
 annotation_BC <- read.table("A-AFFY-33.adf.txt",header=T,sep="\t",skip=17,fill=T)
-print(tail(annotation_BC))
-annotation_BC[100:105,]
 annotation_BC <- annotation_BC[sort(annotation_BC$Composite.Element.Name,index.return=T)$ix,]
 
 ## Check if all probes are present in both sets
@@ -139,7 +143,6 @@ sum(annotation_BC$Composite.Element.Name==sort(rownames(LIMMAout)))
 
 ## Sort LIMMA output alphabetically on probe name
 LIMMAout_sorted <- LIMMAout[sort(rownames(LIMMAout),index.return=T)$ix,]
-dim(LIMMAout_sorted)
 ## Add gene names to LIMMA output
 LIMMAout_sorted$gene <- annotation_BC$Composite.Element.Database.Entry.ensembl.
 LIMMAout_annot <- LIMMAout_sorted[sort(LIMMAout_sorted$adj.P.Val,index.return=T)$ix,]
@@ -152,23 +155,13 @@ dim(LIMMAout_annot)
 significant_pvalues<- LIMMAout_annot[LIMMAout_annot$adj.P.Val<0.05& abs(LIMMAout_annot$logFC) >1,]
 adjusted_pvalues<- LIMMAout_annot[LIMMAout_annot$adj.P.Val<0.05,]
 dim(significant_pvalues) # 40
-dim(adjusted_pvalues) # 
-head(significant_pvalues)
-tail(significant_pvalues)
-
-affyids <- rownames(significant_pvalues)
-
-ensembl <- useMart(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
-output_sign <-getBM(attributes = c('affy_hg_u133_plus_2', 'entrezgene_id','hgnc_symbol','chromosome_name', 'start_position', 'end_position'), #  is a vector of attributes that one wants to retrieve (= the output of the query).
-      filters = 'affy_hg_u133_plus_2', #  is a vector of filters that one wil use as input to the query.
-      values = affyids, # a vector of values for the filters
-      mart = ensembl)
+dim(adjusted_pvalues) # 5333 7
 
 affyids <- rownames(adjusted_pvalues)
 
 ensembl <- useMart(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
-output_sign <-getBM(attributes = c('affy_hg_u133_plus_2', 'entrezgene_id','hgnc_symbol','chromosome_name', 'start_position', 'end_position'), #  is a vector of attributes that one wants to retrieve (= the output of the query).
-                    filters = 'affy_hg_u133_plus_2', #  is a vector of filters that one wil use as input to the query.
+output_sign <-getBM(attributes = c('affy_hg_u133_plus_2', 'entrezgene_id','hgnc_symbol','chromosome_name', 'start_position', 'end_position'),
+                    filters = 'affy_hg_u133_plus_2', #  is a vector of filters that one will use as input to the query.
                     values = affyids, # a vector of values for the filters
                     mart = ensembl)
 
@@ -194,14 +187,20 @@ for (i in rownames(adjusted_pvalues)){
 dim(adjusted_pvalues) # 5028 11
 head(adjusted_pvalues)
 
-save(adjusted_pvalues,file="adjusted_pvalues_annotation_transcription.Rda") # object noemt adjusted_pvalues
-pos_adj <- adjusted_pvalues[adjusted_pvalues$logFC>0,]
-dim(pos_adj) # 3211 
-#expression is higher in tumor than in normal 
-write.table(adjusted_pvalues$hgnc_symbol,file='transcription_genes_adjusted_pvalues.txt',row.names=FALSE,quote=FALSE,col.names=FALSE)
-write.table(pos_adj$hgnc_symbol,file='transcription_genes_adjusted_pvalues_pos.txt',row.names=FALSE,quote=FALSE,col.names=FALSE) # this gives some interesting results
+save(adjusted_pvalues,file="adjusted_pvalues_annotation_transcription.Rda") # object = adjusted_pvalues
 
-# this is only for the probes that have abs(logfold change) >1
+
+
+# for the abs(logFC) > 1
+affyids <- rownames(significant_pvalues)
+
+ensembl <- useMart(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
+output_sign <-getBM(attributes = c('affy_hg_u133_plus_2', 'entrezgene_id','hgnc_symbol','chromosome_name', 'start_position', 'end_position'), 
+                    filters = 'affy_hg_u133_plus_2', #  is a vector of filters that one will use as input to the query.
+                    values = affyids, # a vector of values for the filters
+                    mart = ensembl)
+
+
 significant_pvalues[,"hgnc_symbol"] <-NA
 significant_pvalues[,"chromosome_name"] <-NA
 significant_pvalues[,"start_position"] <-NA
@@ -223,7 +222,5 @@ significant_pvalues2 <- significant_pvalues
 dim(significant_pvalues2) # 39 11
 head(significant_pvalues2)
 
-save(significant_pvalues2,file="Significant_output_annotation_transcription.Rda") # object noemt significant_pvalues2
-load("Significant_output_annotation_transcription.Rda")
+save(significant_pvalues2,file="Significant_output_annotation_transcription.Rda") # object = significant_pvalues2
 sessionInfo()
-
